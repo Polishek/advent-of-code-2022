3fab11cb2f44dac879841d6ee04c23404e8efed7
Polishek <bartder416@student.polsl.pl>
2022-12-05 21:29:53 +0100

    Day 4 / 1

---
+++ advent-of-code/src/main/java/me/polishek/Day4.java
@@ -0,0 +1,60 @@
+package me.polishek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day4 extends Day{
+
+    List<String> listOfSectionPairs;
+
+    int assignmentPair = 0;
+    public Day4(){
+        openScanner("day4/main.txt");
+        listOfSectionPairs = new ArrayList<>(0);
+    }
+    @Override
+    protected void makeTask() {
+
+//================== PART 1 ==================
+//        while(Reader.hasNextLine()){
+//            String line = Reader.nextLine();
+//            int firstSectionLeft = Integer.parseInt(line.substring(0, line.indexOf('-')));
+//            line = line.substring(line.indexOf('-') + 1);
+//            int firstSectionRight = Integer.parseInt(line.substring(0, line.indexOf(',')));
+//            line = line.substring(line.indexOf(',') + 1);
+//            int secondSectionLeft = Integer.parseInt(line.substring(0, line.indexOf('-')));
+//            line = line.substring(line.indexOf('-') + 1);
+//            int secondSectionRight = Integer.parseInt(line);
+//
+//            if((firstSectionLeft <= secondSectionLeft && firstSectionRight >= secondSectionRight) ||
+//                    (firstSectionLeft >= secondSectionLeft && firstSectionRight <= secondSectionRight)){
+//                listOfSectionPairs.add(line);
+//                assignmentPair++;
+//            }
+//        }
+//
+//        System.out.println(listOfSectionPairs.size());
+//        answer = Integer.toString(assignmentPair);
+
+//================== PART 2 ==================
+        while(Reader.hasNextLine()){
+            String line = Reader.nextLine();
+            int firstSectionLeft = Integer.parseInt(line.substring(0, line.indexOf('-')));
+            line = line.substring(line.indexOf('-') + 1);
+            int firstSectionRight = Integer.parseInt(line.substring(0, line.indexOf(',')));
+            line = line.substring(line.indexOf(',') + 1);
+            int secondSectionLeft = Integer.parseInt(line.substring(0, line.indexOf('-')));
+            line = line.substring(line.indexOf('-') + 1);
+            int secondSectionRight = Integer.parseInt(line);
+
+            if((firstSectionLeft < secondSectionLeft && firstSectionRight < secondSectionRight) ||
+                    (firstSectionLeft > secondSectionLeft && firstSectionRight > secondSectionRight)){
+                listOfSectionPairs.add(line);
+                assignmentPair++;
+            }
+        }
+        
+        System.out.println(listOfSectionPairs.size());
+        answer = Integer.toString(assignmentPair);
+    }
+}
