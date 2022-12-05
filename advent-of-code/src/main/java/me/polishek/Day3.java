42c21711f1b1bfa389d2b1902483d0292220acec
Polishek <bartder416@student.polsl.pl>
2022-12-05 01:39:31 +0100

    Day 3 / working

---
+++ advent-of-code/src/main/java/me/polishek/Day3.java
@@ -0,0 +1,42 @@
+package me.polishek;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class Day3 extends Day {
+
+    private int sumOfPriorities = 0;
+    public Day3(){ openScanner("day3/main.txt"); }
+    @Override
+    protected void makeTask() {
+        Set<Character> characterSet = new HashSet<>();
+
+        while (Reader.hasNextLine()){
+            String line = Reader.nextLine();
+            int half = line.length()/2;
+            String firstCompartment = line.substring(0, half);
+            String secondCompartment = line.substring(half);
+
+            for(int i = 0; i < half; i++){
+                char firstCharacter = firstCompartment.charAt(i);
+
+                for(int j = 0; j < half; j++){
+                    char secondCharacter = secondCompartment.charAt(j);
+                    if(firstCharacter == secondCharacter) {
+                        characterSet.add(firstCharacter);
+//                        System.out.println("Added: " + firstCharacter);
+                    }
+                }
+            }
+        }
+
+        answer = Integer.toString(sumOfPriorities(characterSet));
+    }
+
+    private int sumOfPriorities(Set<Character> set){
+        set.forEach(character -> sumOfPriorities += character >= 97 ? character - 96 : character - 38);
+
+        return sumOfPriorities;
+    }
+}
--- advent-of-code/src/main/java/me/polishek/Main.java
+++ advent-of-code/src/main/java/me/polishek/Main.java
@@ -2,8 +2,9 @@ package me.polishek;
 
 public class Main {
     public static void main(String[] args) {
-//        Day1 day = new Day1();
-        Day day2 = new Day2();
-        day2.printAnswer();
+//        Day1 day1 = new Day1();
+//        Day day2 = new Day2();
+        Day day3 = new Day3();
+        day3.printAnswer();
     }
 }
